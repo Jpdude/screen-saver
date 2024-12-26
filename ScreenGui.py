6590c2cf11fa752aaef94be65633303c4d9e1181
Jpdude <johnpaulnjepu08@gmail.com>
2024-12-25 20:28:29 -0800

    Revamed This point where i tried using padding with grid and just after i removed the scrollbar and pack configs

--- ScreenGui.py
+++ ScreenGui.py
@@ -6,9 +6,11 @@ win.title("Window Manager")
 ##win.rowconfigure(index = 1 , weight = 2 , uniform ="u")
 #=====================ENDS=================================
 
-heading_label = Label(win , text = "Detected Windows", font = ("comic sans",20,"bold")).pack(side = TOP , expand = True)
+heading_label = Label(win , text = "Detected Windows(2)", font = ("comic sans",20,"bold")).pack(side = TOP , expand = False , fill = X)
+
 topmost_frame = Frame(win, highlightbackground = "black", highlightthickness = 1 , bg = "red" )
 topmost_frame.pack( side = BOTTOM , expand = True , fill = BOTH)
+
 #heading_label = Label(topmost_frame , text = "Detected Windows", font = ("comic sans",20,"bold")).pack()
 print(topmost_frame.pack_info())
 class Screen:
@@ -19,27 +21,28 @@ class Screen:
     def build(self,row,column):
         print(row,column)
         self.f = Frame(self.master, highlightbackground = "black", highlightthickness = 1 )
-        self.f.rowconfigure(index = 0 , weight = 0 , uniform = "p")
-        self.f.rowconfigure(index = 1 , weight = 1, uniform = "p")
-        self.f.grid(row = row, column = column ,sticky = "nsew" ,padx = 20  )
+        
+        self.f.grid(row = row , column = column , padx = 60 , pady = 20 )
         
         
         viewframe = Frame(self.f , bg = "red" , highlightbackground = "yellow", highlightthickness = 1)
-        viewframe.grid(row = 0 , column = 0 ,sticky = "nsew")
+        viewframe.pack()
 
         view_canvas = Canvas(viewframe ,  width = 500 , height = 300 ,bg = "#000000")
-        view_canvas.pack(fill = "both" , expand = True)
+        view_canvas.pack(side = TOP , expand = False , fill = BOTH)
         
         
 
         toolframe = Frame(self.f,bg = "yellow", highlightbackground = "pink", highlightthickness = 5 )
-        toolframe.grid(row = 1 , column = 0)
+        toolframe.pack()
 
         
-##s = Screen(topmost_f,"hey")
-##s1 = Screen(topmost_f, "heyoo")
-##s2 = Screen(topmost_f, "heyoo")
-##s.build(1,0)
-##s1.build(1,1)
-##s2.build(1,2)
+s = Screen(topmost_frame,"hey")
+s1 = Screen(topmost_frame, "heyoo")
+s2 = Screen(topmost_frame, "heyoo")
+#s3 = Screen(topmost_frame, "hi")
+s.build(1,0)
+s1.build(1,1)
+#s2.build(1,2)
+#s3.build(1,2)
 win.mainloop()        
