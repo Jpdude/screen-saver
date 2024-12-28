e7078595f7633e4a6b356402d0293049ff00d62d
Jpdude <johnpaulnjepu08@gmail.com>
2024-12-28 12:53:09 -0800

    Everything Works perfectly gui wise come back to this point whenever you fuck anything up

--- ScreenGui.py
+++ ScreenGui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog as fd
 win = Tk()
 win.title("Window Manager")
 from PIL import Image , ImageTk
@@ -34,12 +35,43 @@ class Screen:
     def __init__(self,master,screen):
         self.master = master
         self.screen = screen
+
+    def revealrl(self):
+        print("jodfnojhnd")
+        if self.roulette_ckbtn_var.get() == 1:
+            for child in self.static_frameholder.winfo_children():
+                child.configure(state = "disabled")
+                print(child)
+            self.roulette_optLblFrm.pack(pady = 20)
+        else:
+            for child in self.static_frameholder.winfo_children():
+                child.configure(state = "normal")
+            self.roulette_optLblFrm.pack_forget()
+            
     def poop(self,x,y,z):
+        
             self.toolFrameTime.pack_forget()
-            print("poop:",self.typa.get(),y,z,self.master.grid_slaves())
+            self.toolFramePic.pack_forget()
+            
             if self.typa.get() == "Time":
                 self.toolFrameTime.pack(anchor = "nw", pady = 10 , padx = 10)
-                ...
+            elif self.typa.get() == "Pictures":
+                self.toolFramePic.pack(anchor = "nw", pady = 10 , padx = 10)
+    #Note to self rmr to make these functions dynamic in the future ( opendir and openfile) like a variable is passed to them and they just pass on the path to the variable
+    def opendir(self):
+        
+        fname = fd.askdirectory()
+        self.current_dir.set(fname)
+        
+    def openfile(self):
+
+        ft = (("Images","*.jpg"),("Images","*.png"),("Images","*.jpeg"),("Images","*.JPG") )
+        
+        fname = fd.askopenfilename(
+            title = "open a file",
+            filetypes = ft,
+            )
+        self.current_static_pic.set(fname)
     def build(self,row,column):
         print(row,column)
         self.f = Frame(self.master, highlightbackground = "black", highlightthickness = 1 )
@@ -100,10 +132,10 @@ class Screen:
 
         #-------------------------------------------------------------------------------------
         types = LabelFrame(self.toolFrameTime, text = "Types", bg = "#ffffff" )
-        types.pack(pady = (5))
+        types.pack(padx=(0,20))
 
         corner_time = Entry(types, bg = "#ffffff" , width = 7 , textvariable = self.current_time , relief = "flat" )  
-        corner_time.pack(anchor = "ne" , padx = (0,10), pady = (0,10))
+        corner_time.pack(anchor = "ne" , padx = (0,5), pady = (0,5))
         
         ar1 = Frame(types, bg = "#ffffff")
         ar1.pack()
@@ -124,7 +156,7 @@ class Screen:
 
         #-------------------------------------------------------------------------------------
         templating = LabelFrame(self.toolFrameTime, text = "Templating", bg = "#ffffff" )
-        templating.pack(pady = (5))
+        templating.pack(pady = (0,15))
 
         drop_template = OptionMenu(templating , self.temp , *opt_templates)
         drop_template.pack()
@@ -143,6 +175,58 @@ class Screen:
         #-------------------------------------TIME-----------------------------------------------
         #-------------------------------------TIME-----------------------------------------------
         #-------------------------------------TIME-----------------------------------------------
+
+        #-------------------------------------PICTURES-----------------------------------------------
+        #-------------------------------------PICTURES-----------------------------------------------
+        #-------------------------------------PICTURES-----------------------------------------------
+
+        #--------------All Vars and options for option menus-------
+        self.current_static_pic = StringVar()
+        self.current_interval = IntVar()
+        self.current_dir = StringVar()
+
+        optPic = ["Please Choose Template","Time" ,"Pictures","Music"]
+        self.typatempPic = StringVar()
+        self.typatempPic.set(optPic[0])
+        #--------------------END----------------------------
+        #for the Picture choice 
+        self.toolFramePic = Frame(toolframe,bg = "white")
+
+        self.roulette_ckbtn_var = IntVar()
+        self.roulette_ckbtn_var.set(0)
+        
+
+        roulette_ckbtn = Checkbutton(self.toolFramePic , text = "Roulette" , onvalue = 1 , offvalue = 0 ,bg = "white",variable = self.roulette_ckbtn_var,command = self.revealrl)
+        roulette_ckbtn.pack()
+        
+
+        self.static_frameholder = Frame(self.toolFramePic, bg = "#ffffff")
+        self.static_frameholder.pack()
+
+        static_lbl = Label(self.static_frameholder , text  = "Static: " , bg = "#ffffff").pack(side = LEFT)
+        static_ent = Entry(self.static_frameholder , textvariable = self.current_static_pic, bg = "#ffffff")
+        static_ent.pack(side = LEFT)
+        static_fileOpenBtn = Button(self.static_frameholder , text = "Open File" , command = self.openfile, bg = "#ffffff").pack(side = LEFT)
+
+
+        self.roulette_optLblFrm = LabelFrame(self.toolFramePic, bg = "#ffffff")
+        
+        
+        rlbls = ['Interval' , "Directory" , "Templates"]
+        x = 0
+        for lbls in rlbls:
+            lbls_frm = Label(self.roulette_optLblFrm,text = lbls, bg = "#ffffff").grid(row = x , column = 0)
+            x+=1
+
+        interval_ent = Entry(self.roulette_optLblFrm , textvariable = self.current_interval, bg = "#ffffff").grid(row = 0 , column = 1)
+        directory_ent = Entry(self.roulette_optLblFrm , textvariable = self.current_dir, bg = "#ffffff").grid(row = 1 , column = 1)
+        static_fileOpenBtn = Button(self.roulette_optLblFrm , text = "Open File" , command = self.opendir, bg = "#ffffff").grid(row = 1 , column = 2)
+
+        drop_tempPic = OptionMenu(self.roulette_optLblFrm , self.typatempPic , *optPic )
+        drop_tempPic.configure(state="disabled")
+        drop_tempPic.grid(row = 2 , column = 1)
+
+        
 ##        self.toolFrameTime = Frame(toolframe,bg = "white", highlightbackground = "pink", highlightthickness = 5 )
 ##        
 ##        time_config = Label(self.toolFrameTime, text = "Time Configuration" , font=('Helvetica 5 bold'))
